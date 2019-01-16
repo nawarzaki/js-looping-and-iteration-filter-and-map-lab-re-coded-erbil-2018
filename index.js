586b28ff0576accd2b2cf7ca74feba3f12632ef6
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-01-16 14:23:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+let x = drivers.filter(function (c) {
+  return c.revenue >= revenue
+
+})
+return x
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  let y =[]
+let x = drivers.filter(function (c) {
+  if (revenue <= c.revenue){
+    y.push(c.name)
+  }else {
+    []
+  }
+
+})
+return y
+}
+
+function exactMatch(drivers, key) {
+
+  let x = drivers.filter(function (c) {
+let y =[]
+   if (key.key === c.key ){
+    y.push(c.key = key.key)
+  }
+  return  y
+  })
+  return x
+  }
